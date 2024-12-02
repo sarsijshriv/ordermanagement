c4274df0831e55854f9877343bf6b2446aabf866
Sarsij Shrivastava <47062704+sarsijshriv@users.noreply.github.com>
2024-12-02 19:48:08 +0530

    updating security config

--- src/main/java/com/web/ordermanagement/configurations/SecurityConfig.java
+++ src/main/java/com/web/ordermanagement/configurations/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +22,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception{
         return security.formLogin(cus -> cus.disable())
                 .csrf(cus -> cus.disable())
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("register", "login").permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 
